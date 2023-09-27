06d69f150ee48af324731fb8d154b747f2f3b1e8
elyade70 <elhajyade0@gmail.com>
2023-09-27 10:07:45 +0200

    fichier system_hospitalier

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -36,11 +36,13 @@ public class System_hospitalier {
 	private String analyserRequete(Requête requête) {
 		// Analyser la requête
 		return "requête analysée";
+		//renvoie une réponse si la requête est analysée
 	}
 	
 	private String comparerDonnes() {
 		// Comparaison des données
 		return "Cluster trouvé";
+		//renvoie une réponse si un cluster a été trouvé
 	}
 
 }
